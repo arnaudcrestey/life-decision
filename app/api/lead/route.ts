a84540a00559bba66a5427de80db2f529726a227
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-03-16 16:28:14 +0100

    Update route.ts

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { Resend } from "nodemailer";
+import { nodemailer } from "nodemailer";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
